b37294cb62998c6a88d00e70f1d78d26dd4710df
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2023-02-09 18:23:20 +0100

    try2

--- src/socket.js
+++ src/socket.js
@@ -2,9 +2,13 @@ import { io } from "socket.io-client";
 
 const URL = "http://localhost:8000";
 
-const socket = io(URL, {
-    path: "/socket.io",
-    reconnection: false,
+// const socket = io(URL, {
+//     path: "/socket.io",
+//     reconnection: false,
+// });
+
+const socket = io("//url", {
+    transports: ['websocket'],
 });
 
 export default socket
